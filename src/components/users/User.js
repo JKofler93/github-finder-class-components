8f292937079d065a5960e35872ae8f948fd0e83c
github username <github email address>
2021-03-23 12:23:21 -0400

    converted User.js to a functional component and used the useEffect Hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,26 +1,20 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 
 
-export class User extends Component {
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+    // use this when you need to fetch data
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        //eslint-disable-next-line
+        // the line above stops the error from happening in the console
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool, 
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    }
-
-    render() {
         const { 
             name, 
             avatar_url, 
@@ -33,11 +27,8 @@ export class User extends Component {
             followers, 
             following, 
             public_repos, 
-            public_gists, 
-            hireable 
-        } = this.props.user;
-
-        const { loading, repos } = this.props;
+            public_gists
+        } = user;
 
         if(loading) return <Spinner />
 
@@ -48,8 +39,6 @@ export class User extends Component {
                     className="btn btn-light">
                 Back To Search
                 </Link>
-                {/* Hireable: {' '}
-                {hireable ? (<i className="fas fa-check text-success"/>) : (<i className="fas fa-time-circle text-danger"/>) } */}
                 <div className="card grid-2">
                     <div className="all-center">
                         <img src={avatar_url} className="round-img" alt="" style={{width: '150px' }}/>
@@ -95,7 +84,13 @@ export class User extends Component {
                 <Repos repos={repos}/>
             </Fragment>
         )
-    }
 }
 
+User.propTypes = {
+    loading: PropTypes.bool, 
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+}
 export default User
