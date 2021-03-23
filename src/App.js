11f7f86e16169424c00776a945f3027fef4b41d8
github username <github email address>
2021-03-22 20:10:52 -0400

    showign reposin profile page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import User from './components/users/User';
 import Search from './components/users/Search';
+// import Repos from '../repos/Repos';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
@@ -13,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {}, 
+    repos: [],
     loading: false,
     alert: null
   }
@@ -35,11 +37,19 @@ class App extends Component {
   }
 
   // Get a single Github user
-  getUser = async (username) => {
+  getUser = async username => {
     this.setState({ loading: true });
    
     const res = await axios.get(`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ user: res.data, loading: false })
+  }
+
+  // Get single user reports
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+   
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ repos: res.data, loading: false })
   }
 
   // Clear Users from state
@@ -54,7 +64,7 @@ class App extends Component {
 
   render() {
 
-    const { users, loading, user } = this.state
+    const { users, loading, user, repos } = this.state
     return (
       <Router>
         <div className="App">
@@ -86,7 +96,14 @@ class App extends Component {
                 exact
                 path='/user/:login'
                 render={props => (
-                  <User {...props} getUser={this.getUser} user={user} loading={loading}/>
+                  <User 
+                    {...props} 
+                    getUser={this.getUser} 
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                    user={user} 
+                    loading={loading}
+                  />
                 )}
               />
             </Switch>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 
 
@@ -8,12 +9,15 @@ export class User extends Component {
 
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool, 
         user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired
+        repos: PropTypes.array.isRequired,
+        getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired
     }
 
     render() {
@@ -33,7 +37,7 @@ export class User extends Component {
             hireable 
         } = this.props.user;
 
-        const { loading } = this.props;
+        const { loading, repos } = this.props;
 
         if(loading) return <Spinner />
 
@@ -88,6 +92,7 @@ export class User extends Component {
                     <div className="badge badge-light">Public Repos: {public_repos}</div>
                     <div className="badge badge-dark">Public Gists: {public_gists}</div>   
                 </div>
+                <Repos repos={repos}/>
             </Fragment>
         )
     }
